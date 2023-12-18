5b8066707eaa72d57f4ac0414046f601b5a88a0f
Tianjing Li <tianjinglimail@gmail.com>
2023-12-18 14:33:46 -0500

    Elastic cleanup

--- elastic/provider/provider.py
+++ elastic/provider/provider.py
@@ -1,34 +1,22 @@
 import logging
 
-from elasticsearch import Elasticsearch
-from flask import current_app as app
+from .client import get_client
 
 logger = logging.getLogger(__name__)
-es_client = None
 
 MIN_TEXT_LENGTH = 25
 
 
-def create_es_client():
-    connection_params = {}
+def search(query):
+    elasticsearch_client = get_client()
 
-    if cloud_id := app.config.get("CLOUD_ID"):
-        connection_params["cloud_id"] = cloud_id
-    elif url := app.config.get("URL"):
-        connection_params["hosts"] = [url]
-    else:
-        raise ValueError("Either ELASTIC_CLOUD_ID or ELASTIC_URL env vars must be set.")
+    search_results = elasticsearch_client.search(query)
 
-    if api_key := app.config.get("API_KEY"):
-        connection_params["api_key"] = api_key
-    elif (user := app.config.get("USER")) and (password := app.config.get("PASS")):
-        connection_params["basic_auth"] = (user, password)
-    else:
-        raise ValueError(
-            "Either ELASTIC_APIKEY or both ELASTIC_USER and ELASTIC_PASS env vars must be set."
-        )
+    results = []
+    for match in search_results:
+        results.append(serialize_result(match))
 
-    return Elasticsearch(**connection_params)
+    return results
 
 
 def build_text(match):
@@ -56,23 +44,3 @@ def serialize_result(match):
         **stripped_source,
         "text": build_text(match),
     }
-
-
-def search(query):
-    global es_client
-
-    if not es_client:
-        es_client = create_es_client()
-
-    es_query_body = {
-        "query": {"multi_match": {"query": query}},
-        "highlight": {"pre_tags": [""], "post_tags": [""], "fields": {"content": {}}},
-    }
-
-    response = es_client.search(index=app.config["INDEX"], body=es_query_body, size=20)
-
-    results = []
-    for match in response["hits"]["hits"]:
-        results.append(serialize_result(match))
-
-    return results
