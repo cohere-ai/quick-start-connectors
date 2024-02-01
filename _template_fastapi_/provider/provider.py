bbe17e9410948d055b619cd3cff7d80d56cc9fde
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2024-02-01 18:35:41 +0100

    add provider.py

---
+++ _template_fastapi_/provider/provider.py
@@ -0,0 +1,38 @@
+import logging
+from typing import List
+
+from pydantic import ValidationError
+
+from client import get_client
+from datamodels import DataItem
+from exceptions import UpstreamProviderError
+
+
+logger = logging.getLogger(__name__)
+
+
+def search(query: str) -> List[DataItem]:
+    """
+    Handles search requests and returns a list of DataItem objects.
+
+    Args:
+        query (str): Query string
+
+    Returns:
+        List[DataItem]: A list of data items resulting from the search.
+    """
+    client = get_client()
+
+    try:
+        data = client.search(query=query)
+    except Exception as error:
+        logger.error(f"search_error: {error}")
+        raise UpstreamProviderError("Error retrieving data from the search") from error
+
+    try:
+        data_items = [DataItem(**d) for d in data]
+    except ValidationError as error:
+        logger.error(f"Data validation error: {error}")
+        raise UpstreamProviderError("Invalid data format received from the search") from error
+
+    return data_items
