d71138e974074b36e98998cd9d85942aadef4007
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2024-02-01 16:03:35 +0100

    add Exceptions class for UpstreamProviderError

---
+++ _template_fastapi_/provider/exceptions.py
@@ -0,0 +1,12 @@
+"""
+Custom Exceptions
+"""
+class UpstreamProviderError(Exception):
+    """
+    Exception raised when the upstream provider encounters an error.
+    """
+    def __init__(self, message) -> None:
+        self.message = message
+
+    def __str__(self) -> str:
+        return self.message
